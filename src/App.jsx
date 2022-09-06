8520aa302269b9fd2714fda1d9329a1a8276508c
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-09-06 17:12:42 +0200

    updated scss styles and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import "./App.scss";
-import "./scss/reset.scss";
-import "./scss/typography.scss";
-import "./scss/base.scss";
+import "./scss/index.scss";
 import Container from "./components/Container/Container";
 
 function App() {
   return (
     <div className="wrapper">
-      <div class="bg-video">
-        <video class="bg-video__content" autoPlay muted loop>
+      <div className="bg-video">
+        <video className="bg-video__content" autoPlay muted loop>
           <source src={require("./resources/video.mp4")} type="video/mp4" />
           <source src="./img/video.webm" type="video/webm" />
           Your browser is not supported!
