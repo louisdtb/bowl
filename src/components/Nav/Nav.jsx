be87eb3527f43088a04559a5eafcdae3fed12502
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-09-06 18:11:50 +0200

    updated mobile nav logo

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./Nav.scss";
 import { UilMapMarker, UilListUl, UilSearch } from "@iconscout/react-unicons";
 import { ReactComponent as BowlLogo } from "../../resources/bowl-logo.svg";
+import { ReactComponent as BowlLogoMobile } from "../../resources/bowl-logo--mobile.svg";
 
 const Nav = () => {
   return (
     <div className="nav">
       <BowlLogo className="nav__logo"></BowlLogo>
+      <BowlLogoMobile className="nav__logo--mobile"></BowlLogoMobile>
       <ul className="nav__items">
         <li className="nav__item">
           <a href="#" className="nav__link">
